1679de3f6e222fc71046d09b74b42634bc420e94
snaric <snaric01@gmail.com>
2023-07-19 10:43:56 +0200

    first commit

---
+++ src/main/java/shop/ipwebshop/IpWebShopApplication.java
@@ -0,0 +1,13 @@
+package shop.ipwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpWebShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpWebShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/ipwebshop/IpWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.ipwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpWebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
